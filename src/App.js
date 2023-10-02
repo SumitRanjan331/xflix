d22b39c8662f536759ce5d87064baf28ed5ef705
SumitRanjan331 <sumitranjan331@gmail.com>
2023-10-02 09:23:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+import {Routes,Route} from "react-router-dom";
+import LandingPage from "./components/LandingPage.jsx";
+import {ThemeProvider} from "@mui/material";
+import theme from "./theme";
+import VideoPage from "./components/VideoPage.jsx";
+import endPoint from "./config";
+import axios from "axios";
+import { SnackbarProvider } from "notistack";
+import "./css/App.css";
 
-function App() {
+const App = () => {
+  let [videos,setVideos] = useState([]);
+  let [isLoading,setIsLoading] = useState(false);
+
+  const fetchVideos = async(url) => {
+    try{
+      setIsLoading(true);
+      let response = await axios.get(url);
+      let data = response.data.videos;
+      setVideos(data);
+    } catch(error){
+      throw new Error(error);
+    } finally{
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchVideos(endPoint);
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider anchorOrigin={{
+        vertical:"bottom",
+        horizontal:"right",
+      }}>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <LandingPage
+                videos={videos}
+                setVideos={setVideos}
+                isLoading={isLoading}
+                setIsLoading={setIsLoading}
+                fetchVideos={fetchVideos}
+              />
+            }
+          />
+          <Route
+            exact
+            path="/video/:videoId"
+            element={<VideoPage videos={videos} isLoading={isLoading} />}
+          />
+        </Routes>
+      </SnackbarProvider>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default App
---
+++ src/config.js
@@ -0,0 +1,3 @@
+const endPoint =
+  "https://c0122774-f234-450d-9497-9684c1e8a555.mock.pstmn.io/v1/videos";
+export default endPoint;
